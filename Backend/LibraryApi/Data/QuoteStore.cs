110a01213cb3a7566b4f59e5adc2b4bb071e099a
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-04-02 19:28:28 +0200

    create quotes for backend

--- Backend/LibraryApi/Data/QuoteStore.cs
+++ Backend/LibraryApi/Data/QuoteStore.cs
@@ -4,6 +4,10 @@ namespace LibraryApi.Data
 {
     public static class QuoteStore
     {
-        public static List<Quote> Quotes { get; } = new List<Quote>();
+        public static List<Quote> Quotes { get; } = new List<Quote>()
+        {
+            new Quote { Id = 1, Text = "Simplicity is the soul of efficiency." },
+            new Quote { Id = 2, Text = "Code is like humor. When you have to explain it, it’s bad." }
+        };
     }
 }
